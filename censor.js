1a35dd4a2013d0b20f40594aa93c2c6b2f91c040
John <johnhartmann26@gmail.com>
2022-06-20 13:48:03 -0500

    Update censor.js

--- censor.js
+++ censor.js
@@ -1,20 +1,23 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
-client.once('ready', () => {
-	console.log('Ready!');
+client.once("ready", () => {
+  console.log("Ready!");
 });
 
-client.on('message', msg => {
-	console.log(`Message read from ${msg.author.username}`)
-	if (msg.content.toLowerCase().includes("bitch") && msg.author.username !== "hart-bot") {
-        msg.delete()
-        msg.channel.send(`No, you're the bitch. ${msg.author.username} bitch.`)
-	}
-})
+client.on("message", (msg) => {
+  console.log(`Message read from ${msg.author.username}`);
+  if (
+    msg.content.toLowerCase().includes("bitch") &&
+    msg.author.username !== "hart-bot"
+  ) {
+    msg.delete();
+    msg.channel.send(`No. ${msg.author.username} bad.`);
+  }
+});
 
-// DELETE BEFORE PUSHING
-TOKEN = "ODI5NTAzMTYyNzMzMjk3Njc0.YG5FBQ.6QChr2P-J68HwRXvQeAC-OBHN5Q"
-client.login(TOKEN)
+// test token - not to be used in prod
+TOKEN = "ODI5NTAzMTYyNzMzMjk3Njc0.YG5FBQ.6QChr2P-J68HwRXvQeAC-OBHN5Q";
+client.login(TOKEN);
